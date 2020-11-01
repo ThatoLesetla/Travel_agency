7361a25a630c6a19dcf809aec126384080c26270
ThatoLesetla <tlesetla6198@gmail.com>
2020-11-01 20:00:32 +0200

    Add hotel service on frontend

---
+++ frontend/src/app/models/hotel-interface.ts
@@ -0,0 +1,8 @@
+export interface Hotel {
+    hotelID: number,
+    address: string,
+    pricePerNight: number,
+    currency: string,
+    phone: number,
+    email: string
+}
---
+++ frontend/src/app/services/hotel.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { environment} from '../../environments/environment';
+import { Hotel } from '../models/hotel-interface';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelService {
+
+  constructor(private http: HttpClient) { }
+
+
+  // Create Package
+  create(hotel: Hotel): Observable<Hotel> {
+    return this.http.post<Hotel>(`${environment.url}/hotels`, hotel, httpOptions);
+  }
+
+  // Get Packages
+  findAll(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(`${environment.url}/hotels`);
+  }
+
+
+  findOne(): Observable<Hotel> {
+    return this.http.get<Hotel>(`${environment.url}`);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${environment.url}/hotels/${id}`);
+  }
+}
