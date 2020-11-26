49ac3f227ca2caf601afdcf6d5943e3c37558301
ThatoLesetla <tlesetla6198@gmail.com>
2020-11-26 10:30:10 +0200

    bookings tab

--- frontend/src/app/views/bookings/bookings.component.ts
+++ frontend/src/app/views/bookings/bookings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService} from '../../services/booking.service';
+import { Booking } from '../../models/booking-interfact';
 
 @Component({
   selector: 'app-bookings',
@@ -7,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsComponent implements OnInit {
 
+  bookings: Booking[] = [];
   constructor(
+    private bookingService: BookingService
   ) { }
 
   ngOnInit(): void {
+    this.bookingService.findAll().subscribe(data => {
+      this.bookings = data;
+    }, error => {
+      alert('Oops... There is a error retrieving your bookings please try again later');
+    })
   }
 
 
